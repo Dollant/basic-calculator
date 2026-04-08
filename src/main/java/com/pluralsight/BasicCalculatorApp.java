1fd33d2112d9131e740cf9c0e2d1309fa4be3961
Dollant <ariawilliams2425@gmail.com>
2026-04-08 14:21:47 -0400

    Asked for 1st and 2nd number after scanner setup

--- src/main/java/com/pluralsight/BasicCalculatorApp.java
+++ src/main/java/com/pluralsight/BasicCalculatorApp.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class BasicCalculatorApp {
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = keyboard.nextInt();
+        keyboard.nextLine();
+
+        System.out.print("Enter the second number: ");
+        int num2 = keyboard.nextInt();
+        keyboard.nextLine();
+
+
+    }
 }
